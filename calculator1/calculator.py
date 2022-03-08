015245f6d83cc2e7c9dd8c8ff12e6172167f1b25
programmer20000 <84572292+programmer20000@users.noreply.github.com>
2022-03-08 14:54:33 +0200

    project testing

--- calculator1/calculator.py
+++ calculator1/calculator.py
@@ -3,18 +3,20 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.lang.builder import Builder
 from kivy.core.window import Window
 
-
 Builder.load_file("RootLayout.kv")
-Window.size=(400,500)
+Window.size = (400, 500)
+
 
 class RootLayout(BoxLayout):
     pass
 
+
 class MAIN(MDApp):
     def build(self):
-        self.title="Calculator"
-        self.theme_cls.theme_style ="Dark"
+        self.title = "Calculator"
+        self.theme_cls.theme_style = "Dark"
         return RootLayout()
+
 
 if __name__ == '__main__':
     MAIN().run()
